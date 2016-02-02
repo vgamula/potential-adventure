4cc2b2d5e1d94eb6312c442ea733d28207c637dd
Volodymyr Gamula <v.gamula@gmail.com>
2016-02-02 23:49:59 +0200

    Initial commit

---
+++ test/potential_adventure/core_test.clj
@@ -0,0 +1,7 @@
+(ns potential-adventure.core-test
+  (:require [clojure.test :refer :all]
+            [potential-adventure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
