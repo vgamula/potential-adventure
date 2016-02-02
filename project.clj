d5f69af06c3469537b0894121b0042d035b16a98
Volodymyr Gamula <v.gamula@gmail.com>
2016-02-03 01:32:20 +0200

    Added serving static files and some logging

--- project.clj
+++ project.clj
@@ -4,8 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [ring "1.4.0"]
-                 [compojure "1.4.0"]
                  [org.clojure/data.json "0.2.6"]
-                 [http-kit "2.1.18"]]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [compojure "1.4.0"]
+                 [http-kit "2.1.18"]
+                 [ring "1.4.0"]]
   :main potential-adventure.core)
--- src/potential_adventure/core.clj
+++ src/potential_adventure/core.clj
@@ -1,15 +1,19 @@
 (ns potential-adventure.core
   (:use org.httpkit.server
+        [clojure.tools.logging :only [info]]
         (compojure [core :only [defroutes GET]]
                    [handler :only [site]]
-                   [route :only [not-found]])))
+                   [route :only [not-found files]])))
 
 (defn vova-handler [request]
-  "Hello, Vova!")
+  (-> (info "get /vova")
+      (str "Hello, Vova!")))
 
 (defroutes routes
-  (GET "/vova" [] vova-handler))
+  (GET "/vova" [] vova-handler)
+  (files "" {:root "static"}))
 
 
 (defn -main [& args]
-  (run-server (-> #'routes site) {:port 3000}))
+  (run-server (-> #'routes site) {:port 3000})
+  (info "Server running at http://localhost:3000/"))
