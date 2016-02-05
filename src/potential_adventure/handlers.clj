dc26b380bc6b8319a952c6b77f29460f725dea87
Volodymyr Gamula <v.gamula@gmail.com>
2016-02-05 02:45:30 +0200

    Added chatroom via redis pubsub

--- src/potential_adventure/handlers.clj
+++ src/potential_adventure/handlers.clj
@@ -1,21 +1,39 @@
 (ns potential-adventure.handlers
   (:gen-class)
   (:use org.httpkit.server
+        [taoensso.carmine :as car :refer (wcar)]
         [taoensso.timbre :as log]
-        [clojure.data.json :only [read-json]]))
+        [clojure.data.json :only [read-json write-str]]))
+
+(def channel "my-channel")
+(def redis-conn {:pool {} :spec {:host "docker.loc" :port 6379}})
+(defmacro redis [& body] `(car/wcar redis-conn ~@body))
 
 
-(defn data-received [channel msg]
+(defn get-redis-listener [socket]
+  (car/with-new-pubsub-listener (:spec redis-conn)
+    {channel (fn [[type match content]]
+               (send! socket (write-str {:msg content}))
+               (log/info "new pubsub msg:" content))}
+    (car/subscribe channel)))
+
+
+(defn data-received [socket msg]
   (let [data (read-json msg)]
-    (log/info "msg reeived" data "from" channel)))
+    (redis (car/publish channel (get data :msg)))
+    (log/info "New msg received" (get data :msg) "from" socket)))
 
 
-(defn channel-closed [channel status]
-  (log/info "Channel" channel "closed" status))
+(defn socket-closed [socket listener status]
+  (car/with-open-listener listener
+    (car/unsubscribe)
+    (log/info "Disconnected from pubsub"))
+  (log/info "Socket" socket "closed" status))
 
 
 (defn main-ws-handler [request]
-  (with-channel request channel
-    (log/info channel "connected")
-    (on-receive channel (partial data-received channel))
-    (on-close channel (partial channel-closed channel))))
+  (with-channel request socket
+    (def listener (get-redis-listener socket))
+    (on-receive socket (partial data-received socket))
+    (on-close socket (partial socket-closed socket listener))
+    (log/info "New socket connected:" socket)))
